323dbfc1dea544a786ae0ea15962ac996d5971a2
Hassan <91528684+ha33ann@users.noreply.github.com>
2023-04-19 17:02:52 +0200

    Work-in-progress

--- hitch_side_project/lib/Confirmationpage.dart
+++ hitch_side_project/lib/Confirmationpage.dart
@@ -1,2 +1,25 @@
 import 'package:flutter/material.dart';
 
+TextField(
+              keyboardType: TextInputType.visiblePassword,
+              decoration: InputDecoration(
+                hintText: "Email",
+                prefixIcon: Icon(Icons.lock, color: Colors.black),
+              ),
+            ),
+
+            TextField(
+              keyboardType: TextInputType.visiblePassword,
+              decoration: InputDecoration(
+                hintText: "Password",
+                prefixIcon: Icon(Icons.lock, color: Colors.black),
+              ),
+            ),
+
+            TextField(
+              keyboardType: TextInputType.visiblePassword,
+              decoration: InputDecoration(
+                hintText: "You are now a member of the Hitch Family",
+                prefixIcon: Icon(Icons.lock, color: Colors.black),
+              ),
+            ),
